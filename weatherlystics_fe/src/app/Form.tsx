6372127df23b3659db89a750ff5f139acf1da305
Kimuna <lukekliefoth@gmail.com>
2024-05-03 12:47:18 +0200

    chore: Refactor global styles and page component

--- weatherlystics_fe/src/app/Form.tsx
+++ weatherlystics_fe/src/app/Form.tsx
@@ -1,84 +1,119 @@
-
 import { customStyles } from "@/utils/constants";
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { useState } from 'react';
-import Select from 'react-select';
-import styles from '../styles/pages/page.module.scss';
-
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import Select from "react-select";
+import styles from "../styles/pages/page.module.scss";
 
 type FormProps = {
-    location: { latitude: string; longitude: string };
-    DateValue: string;
-    showSecondDate: boolean;
-    toggleSecondDate: (event: React.MouseEvent<HTMLButtonElement>) => void;
-    getGeolocation: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  location: { latitude: string; longitude: string };
+  DateValue: string;
+  showSecondDate: boolean;
+  toggleSecondDate: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  getGeolocation: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Form = ({ location, DateValue, showSecondDate, toggleSecondDate, getGeolocation }:FormProps) => {
-    const [selectedTimezone, setSelectedTimezone] = useState<{ value: string, label: string }>();
-  
-    const { data: timezones, isLoading: isLoadingTimezones } = useQuery({
-        queryKey: ["timezones"],
-        queryFn: async () => {
-            const response = await axios.get<string[]>("http://worldtimeapi.org/api/timezone");
-            return response.data.map((timezone) => ({ value: timezone, label: timezone }));
-        },
-        
-    });
-    const handleTimezoneChange = (selectedOption:{ value: string, label: string }) => {
-        setSelectedTimezone(selectedOption);
-    };
-    return (
-        <form className="form">
-            <div className="form-row">
-                        <label>
-                            Latitude:
-                            <input type="text" name="lat" defaultValue={location?.latitude || ''} />
-                        </label>
-                        <label>
-                            Date:
-                            <input type="date" name="" defaultValue={DateValue} />
-                        </label>
-                        <button className={styles.circleButton} onClick={toggleSecondDate}>
-                            {showSecondDate ? '-' : '+'}
-                        </button>
-                        {showSecondDate && (
-                            <label>
-                            Second Date:
-                            <input type="date" name="secondDate" defaultValue={DateValue} />
-                            </label>
-                        )}
-                    </div>
-                    <div className="form-row">
-                        <label>
-                            Long:
-                            <input type="text" name="long" defaultValue={location?.longitude || ''} />
-                        </label>
-                        
-                        <label>
-                            Timezone:
-                            <Select
-                            styles={customStyles}
-                            options={timezones}
-                            isLoading={isLoadingTimezones}
-                            onChange={handleTimezoneChange}
-                            value={selectedTimezone}
-                            />
-                        </label>
-                    </div>
-                    <div className="form-row">
-                        <button className={styles.normalButton} onClick={getGeolocation}>Use my location</button>
-                        <label>
-                            Pick city
-                            <input type="Text" name="City Picker" />
-                        </label>
-                        <label>
-                            <button className={styles.normalButton} type="submit">Submit</button>
-                        </label>
-                    </div>
-        </form>
-    );
+const Form = ({
+  location,
+  DateValue,
+  showSecondDate,
+  toggleSecondDate,
+  getGeolocation,
+}: FormProps) => {
+  const [selectedTimezone, setSelectedTimezone] = useState<{
+    value: string;
+    label: string;
+  }>();
+
+  const {
+    data: timezones,
+    isLoading: isLoadingTimezones,
+    isError,
+  } = useQuery({
+    queryKey: ["timezones"],
+    queryFn: async () => {
+      const response = await axios.get<string[]>(
+        "http://worldtimeapi.org/api/timezone"
+      );
+      return response.data.map((timezone) => ({
+        value: timezone,
+        label: timezone,
+      }));
+    },
+  });
+  const handleTimezoneChange = (
+    selectedOption: { value: string; label: string } | null
+  ) => {
+    if (selectedOption) {
+      setSelectedTimezone(selectedOption);
+    }
+  };
+
+  if (isLoadingTimezones) return <div>Loading...</div>;
+  if (isError) console.error("Error fetching timezones"); //TODO: Toast
+
+  return (
+    <form className="form">
+      <div className="form-row">
+        <label>
+          Latitude:
+          <input
+            type="text"
+            name="lat"
+            defaultValue={location?.latitude || ""}
+          />
+        </label>
+        <label>
+          Date:
+          <input type="date" name="" defaultValue={DateValue} />
+        </label>
+        <button className={styles.circleButton} onClick={toggleSecondDate}>
+          {showSecondDate ? "-" : "+"}
+        </button>
+        {showSecondDate && (
+          <label>
+            Second Date:
+            <input type="date" name="secondDate" defaultValue={DateValue} />
+          </label>
+        )}
+      </div>
+      <div className="form-row">
+        <label>
+          Long:
+          <input
+            type="text"
+            name="long"
+            defaultValue={location?.longitude || ""}
+          />
+        </label>
+
+        <label>
+          Timezone:
+          <Select
+            styles={customStyles}
+            options={timezones!}
+            isLoading={isLoadingTimezones}
+            onChange={handleTimezoneChange}
+            value={selectedTimezone}
+          />
+        </label>
+      </div>
+      <div className="form-row">
+        <button className={styles.normalButton} onClick={getGeolocation}>
+          Use my location
+        </button>
+        <label>
+          Pick city
+          <input type="Text" name="City Picker" />
+        </label>
+        <label>
+          <button className={styles.normalButton} type="submit">
+            Submit
+          </button>
+        </label>
+      </div>
+    </form>
+  );
 };
 
 export default Form;
--- weatherlystics_fe/src/app/page.tsx
+++ weatherlystics_fe/src/app/page.tsx
@@ -4,74 +4,62 @@ import { useState } from "react";
 import styles from "../styles/pages/page.module.scss";
 import Form from "./Form";
 
-
 const Page = () => {
-    const [location, setLocation] = useState({ latitude: '', longitude: '' });
-    
+  const [location, setLocation] = useState({ latitude: "", longitude: "" });
 
+  const getGeolocation = (event: any) => {
+    event.preventDefault();
 
-    const getGeolocation = (event) => {
-        event.preventDefault();
-    
-        if (typeof window !== 'undefined' && navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(success);
-        } else {
-            console.error('Geolocation is not supported by this browser.');
-        }
-    };
-
-    function success(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-        setLocation({ latitude, longitude });
+    if (typeof window !== "undefined" && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success);
+    } else {
+      console.error("Geolocation is not supported by this browser.");
     }
+  };
 
+  function success(position: any) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setLocation({ latitude, longitude });
+  }
 
-
-  const DateValue = new Date().toISOString().split('T')[0];
+  const DateValue = new Date().toISOString().split("T")[0];
 
   const [showSecondDate, setShowSecondDate] = useState(false);
 
-  const toggleSecondDate = (event) => {
+  const toggleSecondDate = (event: any) => {
     event.preventDefault();
     setShowSecondDate(!showSecondDate);
   };
-  
 
   return (
     <div className={styles.container}>
-    
       <header className={styles.header}>
         <div className="header">Weatherlystics</div>
       </header>
-  
-        <main className={styles.main}>
-            <div className="card">
-                <Form
-                    location={location}
-                    DateValue={DateValue}
-                    showSecondDate={showSecondDate}
-                    toggleSecondDate={toggleSecondDate}
-                    getGeolocation={getGeolocation}
-                />
-            </div>
-            <div className={styles.sectionTwo}>
-                <div className={styles.sectiontwo}>
-                    {/* Ihr JSX hier */}
-                </div>
-            </div>
-            <div className={styles.sectionThree}>
-            {/* Ihr JSX hier */}
-            </div>
-        </main>
-  
+
+      <main className={styles.main}>
+        <div className="card">
+          <Form
+            location={location}
+            DateValue={DateValue}
+            showSecondDate={showSecondDate}
+            toggleSecondDate={toggleSecondDate}
+            getGeolocation={getGeolocation}
+          />
+        </div>
+        <div className={styles.sectionTwo}>
+          <div className={styles.sectiontwo}>{/* Ihr JSX hier */}</div>
+        </div>
+        <div className={styles.sectionThree}>{/* Ihr JSX hier */}</div>
+      </main>
+
       <footer className="footer">
         <div className="footer-content">
           <p>© 2024 Weatherlystics</p>
         </div>
       </footer>
     </div>
-    
   );
 };
 
